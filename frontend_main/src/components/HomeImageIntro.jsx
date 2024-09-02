dfb986bfc55d063d218b6e9a1eeb0b4a7695573e
hao <haochien886@gmail.com>
2024-09-02 22:21:27 +0200

    image testing

--- frontend_main/src/components/HomeImageIntro.jsx
+++ frontend_main/src/components/HomeImageIntro.jsx
@@ -14,7 +14,7 @@ export function HomeImageIntro() {
 
           <Grid.Col span={{ base: 12, md: 7 }}>
             <Image
-            src="https://assets.haodevelop.com/haoweb/home/home_2.jpg"
+            src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
             alt="Running challenge"
             height={400}
             />
